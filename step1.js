ac6115e22efd84634461950abaa71e5e7248f265
Megan Buglio <meganbuglio@Megans-MacBook-Pro.local>
2024-01-25 11:30:24 -0600

    updated files

---
+++ step1.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const process = require('process');
+
+function cat(path){
+    fs.readFile(path, 'utf-8', function(err,data){
+        if (err){
+            console.error(`Error reading ${path}: ${err}`)
+            process.exit()
+        }else{
+            console.log(data);
+        }
+    });
+}
+
+cat(process.argv[2]);
